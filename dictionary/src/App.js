3c1d852bf8a756e3d6e1859e37c00f52816e49ce
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-02-28 11:04:43 +0530

    Added dark mode

--- dictionary/src/App.js
+++ dictionary/src/App.js
@@ -4,8 +4,10 @@ import { useEffect, useState } from 'react';
 import { Container } from '@mui/system';
 import Header from './components/Header/Header.js'
 import Definitions from './components/Definitions/Definitions';
+import { Switch } from '@mui/material';
 function App() {
   const [Word, setWord] = useState("");
+  const [LightMode,setLightMode]=useState(0);
 
   const [Meaning, setMeaning] = useState([]);
   const [category,setCategory]=useState("en");
@@ -13,7 +15,7 @@ function App() {
   const dictinaryApi = async () => {
     try {
       const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${Word}`)
-
+ 
       setMeaning(data.data);
     }
     catch (error) {
@@ -24,10 +26,13 @@ function App() {
     dictinaryApi();
   }, [Word,category]);
   return (
-    <div className="App" style={{height:'100vh',backgroundColor:
-    '#282c34',color:'white'}}>
+    <div className="App" style={{height:'100vh',backgroundColor:LightMode?"#fff":"#282c34",color:LightMode?"#282c34":"#fff", transition:"all 0.5s linear"}}>
       <Container maxWidth="md" style={{diplay:'flex',height:'100vh'}}>
-        <Header category={category} setCategory={setCategory} word={Word} setWord={setWord}/>
+        <div  style={{position:"absolute",top:0,right:15,paddingTop:10}}>
+          <span> {LightMode?"LightMode":"DarkMode"}</span>
+          <Switch  defaultChecked color="default" onChange={()=>setLightMode(!LightMode)} />
+        </div>
+        <Header category={category} setCategory={setCategory} word={Word} setWord={setWord} LightMode={LightMode}/>
         <Definitions word={Word} category={category} Meaning={Meaning}/>
       </Container>
     </div>
--- dictionary/src/components/Header/Header.js
+++ dictionary/src/components/Header/Header.js
@@ -2,14 +2,14 @@ import React from "react";
 import './Header.css'
 import { TextField, createTheme, ThemeProvider, MenuItem } from "@mui/material";
 import categories from './data/Category'
-const Header = ({ setCategory, category, word, setWord }) => {
+const Header = ({ setCategory, category, word, setWord ,LightMode}) => {
     const darkTheme = createTheme({
         palette: {
             primary: {
-                main: '#fff'
+                main: LightMode?'#000':"#fff"
 
             },
-            mode: 'dark',
+            mode: LightMode?'light':'dark',
         },
     });
     const handlechange = (language) => {
